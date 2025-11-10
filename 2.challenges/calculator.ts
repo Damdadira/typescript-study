fe7835f339732a597decf33e2c280b34abff1136
duddk <duddk@scatterx.io>
2025-11-10 14:19:11 +0900

    feat: 좌표 게임 연습

--- 2.challenges/calculator.ts
+++ 2.challenges/calculator.ts
@@ -1,11 +1,11 @@
 /**
  * Let's make a calculator
  */
-console.log(calculate("add", 1, 3));
-console.log(calculate("substract", 3, 1));
-console.log(calculate("multiply", 4, 2));
-console.log(calculate("divide", 4, 2));
-console.log(calculate("remainder", 5, 2));
+console.log(calculate("add", 1, 3));  // 4
+console.log(calculate("substract", 3, 1));  //2
+console.log(calculate("multiply", 4, 2));  //8
+console.log(calculate("divide", 4, 2));  //2
+console.log(calculate("remainder", 5, 2));  //1
 
 type CalculateType = "add" | "substract" | "multiply" | "divide" | "remainder";
 function calculate(type: CalculateType, a: number, b: number): number {
