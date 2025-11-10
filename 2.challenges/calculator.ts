4f58f911ad2e5b9daf98cda714ed29c9003df69c
duddk <duddk@scatterx.io>
2025-11-10 13:40:25 +0900

    계산기 함수 만들기

---
+++ 2.challenges/calculator.ts
@@ -0,0 +1,26 @@
+/**
+ * Let's make a calculator
+ */
+console.log(calculate("add", 1, 3));
+console.log(calculate("substract", 3, 1));
+console.log(calculate("multiply", 4, 2));
+console.log(calculate("divide", 4, 2));
+console.log(calculate("remainder", 5, 2));
+
+type CalculateType = "add" | "substract" | "multiply" | "divide" | "remainder";
+function calculate(type: CalculateType, a: number, b: number): number {
+  switch(type) {
+    case "add":
+      return a + b;
+    case "substract":
+      return a - b;
+    case "multiply":
+      return a * b;
+    case "divide":
+      return a / b;
+    case "remainder":
+      return a % b;
+    default:
+      throw Error("unknown type");
+  }
+}
