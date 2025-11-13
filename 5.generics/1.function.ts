40a5eab6b40053307e2004b4dc1a6f7a7d3f9d7c
duddk <duddk@scatterx.io>
2025-11-13 10:20:45 +0900

    feat: 제네릭 연습(함수)

---
+++ 5.generics/1.function.ts
@@ -0,0 +1,25 @@
+{
+  function checkNotNullBad(arg: number | null): number {
+    if(arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+
+  function checkNotNullAnyBad(arg: any | null): any {
+    if(arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+
+  function checkNotNull<GENERIC>(arg: GENERIC | null): GENERIC {
+    if(arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+
+  console.log(checkNotNull(123));
+  console.log(checkNotNull(true));
+}
